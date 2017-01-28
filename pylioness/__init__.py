8ff5d5aa380841df567c003e822d50cac5cf1d6c
David Stainton <dstainton415@gmail.com>
2017-01-28 17:17:25 +0000

    Add metadata to setup.py and sources

--- pylioness/__init__.py
+++ pylioness/__init__.py
@@ -8,9 +8,15 @@ from __future__ import print_function
 from __future__ import unicode_literals
 from __future__ import with_statement
 
+from pylioness._metadata import __version__, __author__, __contact__
+from pylioness._metadata import __license__, __copyright__, __url__
+
 from pylioness.lioness import AES_SHA256_Lioness, Chacha20_Blake2b_Lioness
 
 __all__ = [
     "AES_SHA256_Lioness",
-    "Chacha20_Blake2b_Lioness"
+    "Chacha20_Blake2b_Lioness",
+
+    "__version__", "__author__", "__contact__",
+    "__license__", "__copyright__", "__url__",
 ]
--- setup.py
+++ setup.py
@@ -5,6 +5,17 @@ from __future__ import print_function
 
 from setuptools import setup
 
+# Hmmmph.
+# So we get all the meta-information in one place (yay!) but we call
+# exec to get it (boo!). Note that we can't "from txtorcon._metadata
+# import *" here because that won't work when setup is being run by
+# pip (outside of Git checkout etc)
+with open('pylioness/_metadata.py') as f:
+    exec(
+        compile(f.read(), '_metadata.py', 'exec'),
+        globals(),
+        locals(),
+    )
 
 description = '''
     python lioness block cipher
@@ -12,7 +23,7 @@ description = '''
 
 setup(
     name='pylioness',
-    version='0.0.1',
+    version=__version__,
     description=description,
     keywords=['python', 'cryptography'],
     install_requires=open('requirements.txt').readlines(),
@@ -25,6 +36,9 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
     ],
-    license="GPLv3",
+    author=__author__,
+    author_email=__contact__,
+    url=__url__,
+    license=__license__,
     packages=["pylioness"],
 )
